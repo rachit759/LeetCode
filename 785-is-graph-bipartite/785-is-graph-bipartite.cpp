1dad686d2f62d84714d7df4004df512d8a2e1019
Rachit Gupta <guptarachit759@gmail.com>
2022-06-02 01:55:25 +0530

    Time: 44 ms (21.79%), Space: 13.6 MB (54.55%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     
-    bool checkbipartite(int start, vector<int> adj[], vector<int> &color) {
-        queue<int> q;
-        q.push(start);
-        color[start] = 0;
-        
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            for(auto it : adj[node]) {
-                if(color[it] == -1) {
-                    q.push(it);
-                    color[it] = !color[node];
-                }
-                else if(color[it] == color[node]) return false;
-            }
-        }
-        return true;
-    }
-    
-    
-    bool checker(int n, vector<int> adj[]) {
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
         vector<int> color(n,-1);
         for(int i = 0; i < n; i++) {
-            if(color[i] == -1) {
-                if(checkbipartite(i,adj,color) == false) return false;
-            }
+           if(color[i] == -1) {
+                queue<int> q;
+                q.push(i);
+                color[i] = 0;
+                while(!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+                    for(auto it : graph[node]) {
+                        if(color[it] == -1) {
+                        q.push(it);
+                        color[it] = !color[node];
+                        }
+                        else if(color[it] == color[node]) return false;
+                    }
+                }
+           }
+
         }
         return true;
-    }
-    
-    bool isBipartite(vector<vector<int>>& graph) {
-        
-        int n = graph.size();
-        vector<int> adj[n];
-        for(int i = 0; i < n; i++) {
-            adj[i] = graph[i];
-        }
-        
-        return checker(n,adj);
-        
-        
     }
 };
