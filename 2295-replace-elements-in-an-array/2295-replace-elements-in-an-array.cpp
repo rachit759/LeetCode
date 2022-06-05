3947b2f7909ecc37417805d5430b88277ec0047a
Rachit Gupta <guptarachit759@gmail.com>
2022-06-06 01:02:31 +0530

    Time: 1085 ms (14.29%), Space: 129.5 MB (71.43%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& oper) {
         
-        map<int,int>  mp;
+        unordered_map<int,int>  mp;
         for(int i = 0; i < nums.size(); i++) {
             mp[nums[i]] = i;
         }
