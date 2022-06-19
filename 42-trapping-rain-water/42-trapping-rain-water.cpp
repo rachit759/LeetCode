06cb56ab589a3deb07b228d56e4a6ffb4c5ff1fc
Rachit Gupta <guptarachit759@gmail.com>
2022-06-20 01:26:00 +0530

    Time: 14 ms (87.52%), Space: 19.8 MB (52.39%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,92 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+        
+//         int n = height.size();
+//         int prefix[n], suffix[n];
+        
+//         prefix[0] = height[0];
+        
+//         for(int i = 1; i<n; i++)
+//         prefix[i] = max(prefix[i-1],height[i]);
+        
+//         suffix[n-1] = height[n-1];           calculate prefix and suffix maximum and then ans 
+                                                 //space complxity order of 2n
+        
+//         for(int i=n-2;i>=0;i--)
+//         suffix[i] = max(suffix[i+1],height[i]);
+        
+//         int water=0;
+        
+//         for(int i=0;i<n;i++)
+//             water+= min(prefix[i],suffix[i]) - height[i];
+        
+//         return water;
+        
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n = height.size();
+        
+        int left = 0;
+        int right = n-1;
+        int leftmax = 0;
+        int rightmax = 0;     // order of n solution using two pointer approach 
+                              // and calculating leftmax and rightmax 
+        int water = 0;
+        while(left < right)
+        {
+            if(height[left] <= height[right])
+            {
+                if(height[left]>=leftmax) leftmax=height[left];
+                else water+= leftmax-height[left];
+                
+                left++;
+            }
+            else
+            {
+                if(height[right]>=rightmax) rightmax=height[right];
+                else water+= rightmax - height[right];
+                
+                right--;
+            }
+        }
+        
+        
+       
+        return water;
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
