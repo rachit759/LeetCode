a2788765918b91f406432ac4886502c6d99063ac
Rachit Gupta <guptarachit759@gmail.com>
2022-06-21 18:48:19 +0530

    Time: 93 ms (62.25%), Space: 17.3 MB (84.31%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,34 +1,23 @@
+// this is a trie question 
+// doing this by brute force
+
+
 class Solution {
 public:
-    // Compare function for the condition of the sorting i.e. according to decreasing length 
-    static bool compare (string& first, string& second) {
-        return first.size() > second.size();
-    }
     int minimumLengthEncoding(vector<string>& words) {
-        unordered_map<string, int> mp;
-        int ans = 0;
         
-        //sorting the array in decreasing length sequence
-        sort(words.begin(), words.end(), compare);
-
-        //Entering each word in hash map.
-        for(int i=0; i<words.size(); i++){
-            mp[words[i]]++;
-        }
         
-        //For each word if it exist in hash map then add its length+1 to ans as # is also included.
-        for(int i=0; i<words.size(); i++){
-            int k = words[i].size();
-            if(mp[words[i]] >0){
-                ans += (k+1);
-            }
-            
-            //Deleting record of any suffix of the word from hash map as it is already covered.
-            for(int j=k-1; j>=0; j--){
-                string s = words[i].substr(j, k-j);
-                mp[s] = 0;
+        unordered_set<string> s(begin(words),end(words));
+        
+        for(auto word : s) {
+            for(int i = 1; i < word.size(); i++) {
+                s.erase(word.substr(i));
             }
         }
-        return ans;
+        
+        int res = 0;
+        
+        for(auto w : s) res += size(w) + 1;
+        return res;
     }
 };
