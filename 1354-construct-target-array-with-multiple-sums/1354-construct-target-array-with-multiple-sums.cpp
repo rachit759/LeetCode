9f168dc69afb195c472c9e5d6472b55133c16109
Rachit Gupta <guptarachit759@gmail.com>
2022-06-25 06:05:36 +0530

    Time: 57 ms (65.47%), Space: 29.6 MB (71.94%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -1,20 +1,30 @@
+#define ll long long 
+#define mod 1e9+7
+#define pb push_back
+#define all(x) (x).begin(),(x).end() 
+
 class Solution {
 public:
-    bool isPossible(vector<int>& target) {
-        priority_queue<int> pq;
-        unsigned int sum = 0;
-        for (int x : target){
-            sum += x, pq.push(x);
-        }
-        while (pq.top() != 1) {
-            int x = pq.top();
+    bool isPossible(vector<int>& nums) {
+        
+        
+        long long sum = accumulate(begin(nums), end(nums), 0LL);
+        priority_queue<ll> pq(begin(nums), end(nums));
+      
+        while(pq.size()) {
+            ll maxele = pq.top();
+            ll remsum = sum - maxele;
             pq.pop();
-            sum-=x;
-            if (x<= sum || sum < 1) return false;
-            x%= sum;
-            sum+=x;
-            pq.push(x?x: sum);
+            
+            if(maxele == 1 or remsum == 1) return 1;
+            if(remsum == 0 or remsum > maxele) return 0;
+            
+            ll updatedmax = maxele % remsum;
+            if(updatedmax == 0) return 0;
+            sum = remsum + updatedmax;
+            pq.push(updatedmax);
+            
         }
-        return true;
+        return 1;
     }
 };
