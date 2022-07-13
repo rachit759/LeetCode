bfdfe33fe7cd9091142992c9bd00d3a1a75279be
Rachit Gupta <guptarachit759@gmail.com>
2022-07-14 03:11:05 +0530

    Time: 436 ms (46.94%), Space: 10.1 MB (48.02%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,58 +1,33 @@
 class Solution {
-	// a,b,c,d are four sides of square
-    int a,b,c,d;
-    bool fun(vector<int>& matchsticks,int i){
-        //Base Case
-        if(i==matchsticks.size()){
-            if(a==0 && b==0 && c==0 && d==0) return true;
-            else return false;
-        }
-        
-		//Now we will explore for all side for given index
-		
-		// if matchstick size is less than side(a or b or c or d)  size , then in that case we will not explore that because that will cause negative side which is not possible
-        if(matchsticks[i]<=a){
-            a-=matchsticks[i];
-            if(fun(matchsticks,i+1)) return true;
-            a+=matchsticks[i];      // backtrack step
-        }
-        
-        if(matchsticks[i]<=b){
-            b-=matchsticks[i];
-            if(fun(matchsticks,i+1)) return true;
-            b+=matchsticks[i];        // backtrack step                    
-        }
-        
-        if(matchsticks[i]<=c){
-            c-=matchsticks[i];
-            if(fun(matchsticks,i+1)) return true;
-            c+=matchsticks[i];         // backtrack step
-        }
-        
-        if(matchsticks[i]<=d){
-            d-=matchsticks[i];
-            if(fun(matchsticks,i+1)) return true;
-            d+=matchsticks[i];         // backtrack step
-        }
-		
-		//If none of the explored option retuen true then  we have to return false
-        return false;
-    }
 public:
-    bool makesquare(vector<int>& matchsticks) {
-		//  if less than four number present in array , then we can not make square
-        if(matchsticks.size()<4) return false;
+    bool dfs(vector<int> &m, int i, int side, int a, int b, int c, int d) {
         
-		// if sum of all number of array is not divisible by 4 , then we can not create a square
-		int sum = accumulate(matchsticks.begin(), matchsticks.end(),0);
-        if(sum % 4 != 0) return false;
+        if(a > side or b > side or c > side or d > side) return 0;
         
-		int sizeSum=sum/4;
-        a=sizeSum,b=sizeSum,c=sizeSum,d=sizeSum;
+        if(i == m.size()) {
+            if(a == side and b == side and c == side and d == side) return 1;
+            return 0;
+        }
         
-		// here we sort our array in reverse order to escape more cases
-		sort(matchsticks.rbegin(), matchsticks.rend());
+        return dfs(m,i+1,side,a+m[i],b,c,d)
+        or dfs(m,i+1,side,a,b+m[i],c,d)
+       or dfs(m,i+1,side,a,b,c+m[i],d)
+       or dfs(m,i+1,side,a,b,c,d+m[i]);
+      
+    }
+    
+    
+    
+    
+    bool makesquare(vector<int>& m) {
         
-		return fun(matchsticks,0);
+        if(m.size() < 4) return 0;
+        int sum = accumulate(begin(m), end(m), 0);
+        if(sum % 4 != 0) return 0;
+        sum /= 4;
+        sort(rbegin(m),rend(m));
+        return dfs(m,0,sum,0,0,0,0);
+        
+        
     }
 };
