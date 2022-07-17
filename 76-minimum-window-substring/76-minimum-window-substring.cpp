bafd6e194efe7ce06e6809cbbc00c76d4ab0ccf9
Rachit Gupta <guptarachit759@gmail.com>
2022-07-18 03:08:27 +0530

    Time: 29 ms (56.43%), Space: 7.7 MB (83.74%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        unordered_map<char,int>  mp;
+        
+        for(char i : t) mp[i]++;
+        
+        int i(0), j(0), start, len(INT_MAX), counter(size(t)), n(size(s));
+        
+        
+        while(j < n) {
+            --mp[s[j]];
+            if(mp[s[j]] >= 0) --counter;
+            
+            
+            while(counter == 0) {
+                if(j - i + 1 < len) {
+                    len = j - i + 1;
+                    start = i;
+                }
+                
+                mp[s[i]]++;
+                if(mp[s[i]] > 0) ++counter;
+                i++;
+            }
+            
+            
+            j++;
+        }
+        
+        //return "";
+        return len == INT_MAX ? "" : s.substr(start,len);
+        
+        
+    }
+};
