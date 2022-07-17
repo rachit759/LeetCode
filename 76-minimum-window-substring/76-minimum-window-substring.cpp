7fb63880af2f61774b58dcd2e7987b3d0ff2812a
Rachit Gupta <guptarachit759@gmail.com>
2022-07-18 03:16:16 +0530

    Time: 25 ms (64.53%), Space: 7.7 MB (83.74%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -8,30 +8,22 @@ public:
         
         int i(0), j(0), start, len(INT_MAX), counter(size(t)), n(size(s));
         
-        
         while(j < n) {
             --mp[s[j]];
             if(mp[s[j]] >= 0) --counter;
-            
-            
-            while(counter == 0) {
+            while(i < n and counter == 0) {
                 if(j - i + 1 < len) {
                     len = j - i + 1;
                     start = i;
                 }
-                
                 mp[s[i]]++;
                 if(mp[s[i]] > 0) ++counter;
                 i++;
             }
-            
-            
             j++;
         }
         
-        //return "";
         return len == INT_MAX ? "" : s.substr(start,len);
-        
-        
+         
     }
 };
