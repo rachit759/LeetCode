b914345aeb49caa4c980cbffe353fcad63c42308
Rachit Gupta <guptarachit759@gmail.com>
2022-07-19 04:33:36 +0530

    Time: 209 ms (50.00%), Space: 68.5 MB (25.00%) - LeetHub

---
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -0,0 +1,33 @@
+// Explanation
+// "divides all the elements of numsDivide"
+// equals to
+// "divides gcd of all the elements of numsDivide"
+// where gcd is the greatest common divisor.
+
+// So the first step, find out the gcd of numsDivide,
+// then sort input nums A,
+// and find out the smallest element A[i] in A divides gcd.
+
+// We need to remove all elements smaller than A[i].
+// If no such A[i], return -1
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        
+        int g = numsDivide[0];
+        for(auto i : numsDivide) 
+            g = gcd(g,i);
+        
+        sort(begin(nums),end(nums));
+        
+        
+        for(int i = 0; i < size(nums) and nums[i] <= g; i++) {
+            if(g % nums[i] == 0) return i;
+        }
+        
+        return -1;
+        
+    }
+};
