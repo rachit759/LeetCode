25557c4791031d86892abe7a6a42bd3a89dc3796
Rachit Gupta <guptarachit759@gmail.com>
2022-07-25 14:52:48 +0530

    Time: 184 ms (37.76%), Space: 58.5 MB (30.86%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        
+        if(size(nums) < 3) return false;
+        
+        unordered_map<int,int>  mp, hp;   // freq map // hypothetical map
+        
+        for(auto ele : nums) mp[ele]++; 
+         
+        for(auto ele : nums) {
+            if(mp[ele] == 0) continue;
+            
+            if(hp[ele] > 0) {
+                hp[ele]--;
+                hp[ele+1]++;
+                mp[ele]--;
+            }
+            else if(mp[ele] > 0 and mp[ele+1] > 0 and mp[ele+2] > 0) {
+                mp[ele]--;
+                mp[ele+1]--;
+                mp[ele+2]--;
+                hp[ele+3]++;
+            }
+            
+            else return false;
+            
+                 
+        }
+        
+        return true;
+        
+    }
+};
