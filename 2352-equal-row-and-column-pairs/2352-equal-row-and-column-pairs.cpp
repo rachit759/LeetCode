922c1a097bcf365f7d7e31876fb37f0685aae90c
Rachit Gupta <guptarachit759@gmail.com>
2022-07-26 02:43:32 +0530

    Time: 183 ms (66.67%), Space: 32.7 MB (33.33%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,26 +1,49 @@
+// class Solution {
+// public:
+//     int equalPairs(vector<vector<int>>& grid) {
+        
+//         int n(size(grid));
+        
+//         vector<vector<int>> ans = grid;
+        
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i; j < n; j++) {
+// 	            if(i == j) continue;
+//                 swap(grid[i][j],grid[j][i]);
+//             }
+//         }
+        
+//         int count = 0;
+        
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+// 	            if(ans[i] == grid[j]) count += 1;
+//             }
+//         }
+        
+//         return count;
+//     }
+// };
+
+// better soln
+
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        
-        int n(size(grid));
-        
-        vector<vector<int>> ans = grid;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-	            if(i == j) continue;
-                swap(grid[i][j],grid[j][i]);
-            }
+        map<vector<int>, int> rows;
+        for (const auto& row : grid) {
+            rows[row]++;
         }
         
-        int count = 0;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-	            if(ans[i] == grid[j]) count += 1;
+        int ans = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            vector<int> col;
+            for (int j = 0; j < grid.size(); j++) {
+                col.push_back(grid[j][i]);
             }
+            ans += rows[col];
         }
         
-        return count;
+        return ans;
     }
 };
