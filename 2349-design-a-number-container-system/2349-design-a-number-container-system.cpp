45e532e5d41e1c29fa7c0f83cc3b07b165237c70
Rachit Gupta <guptarachit759@gmail.com>
2022-07-25 17:29:29 +0530

    Time: 573 ms (66.67%), Space: 177 MB (33.33%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -9,7 +9,6 @@ public:
     void change(int index, int number) {
         if(imp.find(index) != imp.end()) {
             mp[imp[index]].erase(index);
-            if(mp[imp[index]].empty()) mp.erase(imp[index]);
         }
         imp[index] = number;
         mp[number].insert(index);
@@ -18,11 +17,31 @@ public:
     int find(int number) {
         
         if(mp.find(number) != mp.end()) {
-            return *mp[number].begin();
+            return mp[number].empty() ? -1 : *mp[number].begin();
         }
         return -1;
     }
 };
+
+
+
+
+// class NumberContainers {
+// public:
+//     unordered_map<int, int> ind_num;
+//     unordered_map<int, set<int>> num_inds;
+//     void change(int index, int number) {
+//         auto it = ind_num.find(index);
+//         if (it != end(ind_num))
+//             num_inds[it->second].erase(index);
+//         ind_num[index] = number;
+//         num_inds[number].insert(index);
+//     }
+//     int find(int number) {
+//         auto it = num_inds.find(number);
+//         return it == end(num_inds) || it->second.empty() ? -1 : *begin(it->second);
+//     }
+// };
 
 /**
  * Your NumberContainers object will be instantiated and called as such:
