3b19db8d4a9bc22ff34eeac9f20630d9ab5aa606
Rachit Gupta <guptarachit759@gmail.com>
2022-07-25 15:18:56 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (97.13%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSwapsCouples(vector<int>& row) {
+        
+        
+        for(auto &i : row) {           // making couples equal
+            if(i & 1) --i;
+        }
+        
+        int n(size(row)), swaps(0);
+        
+        for(int i = 0; i < n; i += 2) {
+            if(row[i] == row[i+1]) continue;
+            
+            int j = i+2;
+            
+            while(j < n) {
+                if(row[i] == row[j]) break;
+                j++;
+            }
+            swaps += 1;
+            swap(row[i+1], row[j]);
+        }
+        
+        
+        return swaps;
+        
+    }
+};
