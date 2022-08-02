af4635324865ed557a9729fb9a96f92208aa2674
Rachit Gupta <guptarachit759@gmail.com>
2022-08-02 15:17:52 +0530

    Time: 41 ms (5.10%), Space: 11.1 MB (88.07%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,84 +1,29 @@
-class cmp{
-public:
-    const int sums(int ele) const{
-        int sum = 0;
-        while(ele) {
-            sum += ele%10;
-            ele/=10;
-        }
-        return sum;
-    }
-    
-   bool operator()(const int &a, const int &b) const {
-        
-       string t1 = to_string(a) + to_string(b);
-       string t2 = to_string(b) + to_string(a);
-       
-       return stoll(t1) > stoll(t2);
-       
-    }
-};
-
-
-
-
 class Solution {
 public:
     
+    static bool cmp(const int &a, const int &b) {
+        
+        string t1 = to_string(a) + to_string(b);
+        string t2 = to_string(b) + to_string(a);;
+        return stoll(t1) > stoll(t2);
+        
+        
+    }
+    
+    
     string largestNumber(vector<int>& nums) {
         
-//         int n(size(nums));
-//         int i;
-//         for(i = 0; i < n; i++) {
-//             if(nums[i] != 0) break;
-//         }
         
-        
-//         if(i == n) return 0;
-        
-        map<int,multiset<int,cmp>>  mp;
-        int flag = 1;
-        for(int i = 0; i < size(nums); i++) {
-            if(nums[i] != 0) flag = 0;
-            string temp = to_string(nums[i]);
-            mp[temp[0]-'0'].insert(nums[i]);
-        }
-        
+        sort(begin(nums),end(nums),cmp);
         
         string ans;
-    
         
-        for(auto it = mp.rbegin(); it!=mp.rend(); it++) {
-            
-//            if(it->first == 0) {
-               
-//                if(flag == 1) ans+="0";
-//                else {
-//                     auto s = it->second;
-//                    ans += string(s.size(),'0');
-//                }
-               
-//                continue;
-//            } 
-            
-           auto s = it->second;
-            for(auto ele : s) {
-                //cout<<ele<<" ";
-                ans += to_string(ele);
-            }
-            
-            
+        
+        for(int i = 0; i < nums.size(); i++) {
+            ans += to_string(nums[i]);
         }
         
-        
-        if(ans[0]=='0')
-            ans="0";
-        
-        return ans;
-        
-        
-        
-        
+        return ans[0] == '0' ? "0" : ans;
         
     }
 };
