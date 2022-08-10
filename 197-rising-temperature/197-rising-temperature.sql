ae1f8e9e8d0879df9c8554621e12c8eb511ef825
Rachit Gupta <guptarachit759@gmail.com>
2022-08-10 18:55:50 +0530

    Time: 433 ms (81.87%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+
+select w1.id
+from weather w1 , weather w2
+where w1.temperature > w2.temperature and to_days(w1.recorddate)-to_days(w2.recorddate) = 1;
+
