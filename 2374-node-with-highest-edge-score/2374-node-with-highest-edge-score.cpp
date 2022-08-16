251cef0aab7932c818dac676eb68280ea0b85627
Rachit Gupta <guptarachit759@gmail.com>
2022-08-16 22:55:28 +0530

    Time: 361 ms (52.30%), Space: 109.9 MB (66.70%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     int edgeScore(vector<int>& edge) {
         
-        unordered_map<int,long long int>  mp;
+//         unordered_map<int,long long int>  mp;
         
-        for(int i = 0; i < size(edge); i++) {
+//         for(int i = 0; i < size(edge); i++) {
+//             mp[edge[i]] += i;
+//         }
+        
+//         int maxi = INT_MIN;
+//         int index = INT_MAX;
+            
+//         for(auto it : mp) {
+//             if(it.second > maxi) {
+//                 index = it.first;
+//                 maxi = it.second;
+//             }
+//             else if(it.second == maxi and it.first < index) {
+//                 index = it.first;
+//             }
+//         }
+        
+//         return index;
+        
+        int n(size(edge));
+        
+        vector<long long> mp(n);
+        
+        for(int i = 0; i < n; i++) {
             mp[edge[i]] += i;
         }
         
-        int maxi = INT_MIN;
-        int index = INT_MAX;
-            
-        for(auto it : mp) {
-            if(it.second > maxi) {
-                index = it.first;
-                maxi = it.second;
-            }
-            else if(it.second == maxi and it.first < index) {
-                index = it.first;
-            }
-        }
-        
-        return index;
-        
+        return max_element(begin(mp),end(mp)) - begin(mp);
     }
 };
