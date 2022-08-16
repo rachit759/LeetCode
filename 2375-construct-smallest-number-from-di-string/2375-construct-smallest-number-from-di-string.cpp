18a7815a72dd127ab2927a9eabb84e02c2cd5d95
Rachit Gupta <guptarachit759@gmail.com>
2022-08-17 04:58:14 +0530

    Time: 191 ms (14.88%), Space: 11.7 MB (7.85%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    
+    vector<string> ans;
+    int n;
+    int j;
+    string smallestNumber(string pattern) {
+        
+        n = pattern.length();
+        j = 0;
+        vector<int> nums(n+1), vis(n+2,0);        
+        iota(begin(nums),end(nums),1);
+        
+        string vec;
+        
+        helper(nums,vis,vec,pattern);
+         vector<string> ss;
+       
+        //  for(auto it : ans) {
+        //     cout<<it<<" ";
+        // }
+       
+        int maxi = INT_MAX;
+        
+        for(auto it : ans) {
+            maxi = min(stoi(it),maxi);
+        }
+        
+        return to_string(maxi);
+    }
+    
+    void helper(vector<int> &nums, vector<int> &vis, string vec, string pat) {
+        
+        if(vec.size() == n + 1) {
+            ans.push_back(vec);
+            return;
+        }
+        
+        for(int i = 0; i < size(nums); i++) {
+            if(!vis[nums[i]]) { 
+                
+                if(vec.size() == 0) {
+                    vec += to_string(nums[i]);
+                vis[nums[i]] = 1;
+                helper(nums,vis,vec,pat);
+                vec.pop_back();
+                vis[nums[i]] = 0;
+                }
+                
+                else if((pat[j] == 'D' and nums[i] < vec.back()-'0')  or (pat[j] == 'I' and nums[i] > vec.back()-'0'))  {
+                j++;    
+                 vec += to_string(nums[i]);
+                vis[nums[i]] = 1;
+                helper(nums,vis,vec,pat);
+                 vec.pop_back();
+                vis[nums[i]] = 0;
+                j--;
+                }
+            }
+        }
+        
+    }
+    
+};
