926757bb508a95b7f9b4111f24ad10085d532c8f
Rachit Gupta <guptarachit759@gmail.com>
2022-08-16 22:51:13 +0530

    Time: 620 ms (28.90%), Space: 139.8 MB (21.22%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int edgeScore(vector<int>& edge) {
         
-        map<int,long long int>  mp;
+        unordered_map<int,long long int>  mp;
         
         for(int i = 0; i < size(edge); i++) {
             mp[edge[i]] += i;
         }
         
         int maxi = INT_MIN;
-        int index;
+        int index = INT_MAX;
             
         for(auto it : mp) {
             if(it.second > maxi) {
                 index = it.first;
                 maxi = it.second;
+            }
+            else if(it.second == maxi and it.first < index) {
+                index = it.first;
             }
         }
         
