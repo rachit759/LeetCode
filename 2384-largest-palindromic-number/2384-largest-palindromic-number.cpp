51dd60423c593189b428742f88877959c2f6976c
Rachit Gupta <guptarachit759@gmail.com>
2022-08-21 19:20:52 +0530

    Time: 24 ms (100.00%), Space: 14.1 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -3,43 +3,26 @@ public:
     string largestPalindromic(string num) {
         
         vector<int> vis(10,0);
-        int zero =0;
         for(auto it : num) vis[it-'0']++; 
 
-    
         string temp;
-        bool flag = 0;
-        int pos;
-        
+        int flag = -1;
         
         for(char i = '9'; i >= '0'; i--) {
             
-            if(vis[i-'0']&1 and !flag) {
-                flag = true;
-                pos = i - '0';
-            }
-           
-            if(vis[i-'0'] >= 2 and i != '0') {
-                int c = vis[i-'0']/2;
-                while(c--) temp += char(i);
-            }
-               if(temp.size() and i =='0') {
-             int c = vis[0]/2;
-                while(c--) temp += char('0');
-        }
-        
-            
+            if(vis[i-'0'] & 1 and flag == -1) flag = i-'0';   // first greatest odd
+            if(vis[i-'0'] >= 2 and i != '0') temp += string(vis[i-'0']/2,i);   // when even not zero
+            if(temp.size() and i =='0') temp += string(vis[i-'0']/2,i); //  only zero when nothing is added
         }
         
      
-        if(temp.empty() and !flag) return "0";
+        if(temp.empty() and flag == -1) return "0";   // when complete string is zero
        
-        string rev = temp;
+        string front = temp;
         reverse(temp.begin(),temp.end());
         
-        if(flag == true) 
-        return rev + to_string(pos) + temp;
-        return rev + temp;
+        if(flag != -1) return front + to_string(flag) + temp;  // when we have any odd
+        return front + temp;  
         
     }
 };
