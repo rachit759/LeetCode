668fb58ef2bf74eedd5761af087f555fbd44a766
Rachit Gupta <guptarachit759@gmail.com>
2022-08-29 03:08:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (42.82%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     vector<int> freq(int n) {
         vector<int> mp(10,0);
         while(n) {
@@ -10,16 +9,11 @@ public:
         return mp;
     }
     
-    
-    
     bool reorderedPowerOf2(int n) {
-        
         vector<int> check = freq(n);
-        
-        for(int i =0 ; i <= 29; i++) {
+        for(int i = 0; i <= 29; i++) {    // 29 is because 2 to the power 29 is less than 10^9 and 2^30 is greater than 10^9
             if(check == freq(1<<i)) return true;
         }
-        
         return false;
     }
 };
