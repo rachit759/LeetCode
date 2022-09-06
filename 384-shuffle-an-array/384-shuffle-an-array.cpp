fddd669f14260832e1d110f6ba763bbebbc7c9b6
Rachit Gupta <guptarachit759@gmail.com>
2022-09-06 05:35:30 +0530

    Time: 154 ms (8.71%), Space: 56.9 MB (90.21%) - LeetHub

---
+++ 384-shuffle-an-array/384-shuffle-an-array.cpp
@@ -0,0 +1,36 @@
+// fisher yate algorithm
+// shuffling algorithm
+// works by swapping
+// https://youtu.be/4zx5bM2OcvA
+
+
+class Solution {
+public:
+    vector<int> original;
+    Solution(vector<int>& nums) {
+        original = nums;
+    }
+    
+    vector<int> reset() {
+        return original;
+    }
+    
+    vector<int> shuffle() {
+        vector<int> temp = original;
+        int n(size(original));
+        
+        for(int i = n-1; i >= 0; i--) {
+            int j = rand()%(i+1);
+            swap(temp[i],temp[j]);
+        }
+        
+        return temp;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
