c422db39c3b748d6b4e00230da37a44aff9eeb2b
Rachit Gupta <guptarachit759@gmail.com>
2022-09-20 00:06:21 +0530

    Time: 842 ms (20.00%), Space: 110.8 MB (40.00%) - LeetHub

---
+++ 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long minimumMoney(vector<vector<int>>& t) {
+        
+        int n = t.size();
+        long long loss = 0;
+        int maxa = 0;
+        for(int i = 0; i < n; i++) {
+            if(t[i][0] > t[i][1]) {
+                loss += t[i][0] -t[i][1];
+                maxa = max(maxa,t[i][1]);
+            }
+            else maxa = max(maxa,t[i][0]);
+        }
+        
+        return loss + maxa;
+    }
+};
