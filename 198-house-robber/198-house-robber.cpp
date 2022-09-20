786f6071a54478cc9ba6574f696bff453d4bb9c6
Rachit Gupta <guptarachit759@gmail.com>
2022-09-21 05:07:31 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.91%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     
-    int dfs(int i, vector<int>& nums, vector<int>& dp) {
-        if(i == 0) return nums[i];
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
-        int pick = nums[i] + dfs(i-2,nums,dp);
-        int notpick = dfs(i-1,nums,dp);
-        return dp[i] = max(pick,notpick);
-    }
+//     int dfs(int i, vector<int>& nums, vector<int>& dp) {
+//         if(i == 0) return nums[i];
+//         if(i < 0) return 0;
+//         if(dp[i] != -1) return dp[i];
+//         int pick = nums[i] + dfs(i-2,nums,dp);
+//         int notpick = dfs(i-1,nums,dp);
+//         return dp[i] = max(pick,notpick);
+//     }
     
     
     
@@ -19,6 +19,18 @@ public:
     int rob(vector<int>& nums) {
         int n(size(nums));
         vector<int> dp(n,-1);
-        return dfs(nums.size()-1,nums,dp);
+        
+        dp[0]= nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            
+            int pick = nums[i];
+            if(i > 1) pick += dp[i-2];
+            int notpick = 0 + dp[i-1];
+            dp[i] = max(pick,notpick);
+        }
+        
+        return dp[n-1];
+        //return dfs(nums.size()-1,nums,dp);
     }
 };
